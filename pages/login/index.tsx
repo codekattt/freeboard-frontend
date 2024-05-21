d05da9ad5b2a74b8af97826a25d212660e454e0e
codekattt <widkid00@naver.com>
2024-05-21 15:48:01 +0900

    login_path_correction

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,4 @@
-import LoginPage from '../../src/components/units/logIn/login';
+import LoginPage from '../../src/components/units/logIn/LogIn.index';
 
 export default function Login(): JSX.Element {
   return <LoginPage />;
